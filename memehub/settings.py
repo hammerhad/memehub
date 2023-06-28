fc67fa7c12e2717179e602c3ed9a862d31b48a27
hammerhad <sbasharuli@gmail.com>
2023-06-28 13:55:39 +0400

    debugging

--- memehub/settings.py
+++ memehub/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'mhub/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/'),
+    os.path.join(BASE_DIR, 'mhub/static/'),
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
