501212f17df593f9e17748cadae1f1b6d141b99d
hammerhad <sbasharuli@gmail.com>
2023-06-28 13:51:07 +0400

    staticfiles error debugging

--- memehub/settings.py
+++ memehub/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/collected_static/'
+STATIC_URL = 'mhub/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static/')
 
