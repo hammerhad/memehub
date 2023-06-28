8f668bfa27ac36138066f4a5f6cdc8c65afe58d5
hammerhad <sbasharuli@gmail.com>
2023-06-28 12:08:05 +0400

    database = postgres

--- memehub/settings.py
+++ memehub/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'memehub.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'memehub',
+        'USER': 'admin',
+        'PASSWORD': 'Basharuli12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'memehub',
-            'USER': 'admin',
-            'PASSWORD': 'Basharuli12345',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
 }
 
 # Password validation
