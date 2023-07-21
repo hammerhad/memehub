bf19e3e8eb8786c78228dbc8e191a2ecc78c279d
hammerhad <sbasharuli@gmail.com>
2023-07-22 02:42:32 +0400

    push

--- memehub/settings.py
+++ memehub/settings.py
@@ -76,24 +76,24 @@ WSGI_APPLICATION = 'memehub.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mhub',
-        'USER': 'admin',
-        'PASSWORD': 'Basharuli12345',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mhub',
+#         'USER': 'admin',
+#         'PASSWORD': 'Basharuli12345',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
